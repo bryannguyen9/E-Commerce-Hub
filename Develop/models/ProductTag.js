efc6274a8aaeef3f54c3adae417b27edb1d90b10
bryannguyen9 <127802656+bryannguyen9@users.noreply.github.com>
2023-05-02 19:22:49 -0700

    installed necessary dependencies, defined columns in models

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -7,6 +7,27 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      // tag_id is a foreign key that references the primary key of the tag model
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
